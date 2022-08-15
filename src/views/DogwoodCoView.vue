d5afc11d92ee6df7a8719c48f52062455f9d4a45
JackFlannery <jackryanflannery@gmail.com>
2022-08-15 13:49:44 -0400

    Added routing to project and got some initial page views setup

---
+++ src/views/DogwoodCoView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Dogwood Co. Logo" src="../assets/dogwoodco.png">
+    <HelloWorld msg="Dogwood Co."/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
